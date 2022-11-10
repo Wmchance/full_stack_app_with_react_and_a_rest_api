665b9a24964e8fd402b94eb07bcf038d9d7e9572
Will <wmchance@gmail.com>
2022-11-10 14:05:47 +0700

    add actions bar to CourseDetails component

--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -30,31 +30,40 @@ const CourseDetails = () => {
     ) 
 
     return (
-        <div className="wrap">
-            <h2>Course Detail</h2>
-            <form>
-                <div className="main--flex">
-                    <div>
-                        <h3 className="course--detail--title">Course</h3>
-                        <h4 className="course--name">{courseInfo.title}</h4>
-                        <p>By {courseInfo.User?.firstName} {courseInfo.User?.lastName}</p> {/* ? used to check that User isn't Null before getting lastName - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining */}
-                        <ReactMarkdown 
-                            children={courseInfo.description}
-                        />
-                    </div>
-                    <div>
-                        <h3 className="course--detail--title">Estimated Time</h3>
-                        <p>{courseInfo.estimatedTime}</p>
-
-                        <h3 className="course--detail--title">Materials Needed</h3>
-                        <ReactMarkdown 
-                            className="course--detail--list"
-                            children={courseInfo.materialsNeeded}
-                        />
-                    </div>
+        <React.Fragment>
+            <div className="actions--bar">
+                <div className="wrap">
+                    <a className="button" href="/update-course">Update Course</a>
+                    <a className="button" href="delete.html">Delete Course</a>
+                    <a className="button button-secondary" href="/">Return to List</a>
                 </div>
-            </form>
-        </div>
+            </div>
+            <div className="wrap">
+                <h2>Course Detail</h2>
+                <form>
+                    <div className="main--flex">
+                        <div>
+                            <h3 className="course--detail--title">Course</h3>
+                            <h4 className="course--name">{courseInfo.title}</h4>
+                            <p>By {courseInfo.User?.firstName} {courseInfo.User?.lastName}</p> {/* ? used to check that User isn't Null before getting lastName - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining */}
+                            <ReactMarkdown 
+                                children={courseInfo.description}
+                            />
+                        </div>
+                        <div>
+                            <h3 className="course--detail--title">Estimated Time</h3>
+                            <p>{courseInfo.estimatedTime}</p>
+
+                            <h3 className="course--detail--title">Materials Needed</h3>
+                            <ReactMarkdown 
+                                className="course--detail--list"
+                                children={courseInfo.materialsNeeded}
+                            />
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </React.Fragment>
     )
 
 }
