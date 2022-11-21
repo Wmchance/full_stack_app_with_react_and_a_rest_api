8937a2732e0dc0b24bff5344b15aa454f72f786c
Will <wmchance@gmail.com>
2022-11-21 15:46:06 +0700

    add dynamic info to header based on if signed in

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { AuthConsumer } from "./Context";
+
 
 const Header = () =>{
     return (
@@ -8,14 +10,25 @@ const Header = () =>{
             <div className="wrap header--flex">
                 <h1 className="header--logo"><Link to="/">Courses</Link></h1>
                 <nav>
-                    <ul className="header--signedout">
-                        <li><Link to="/signup">Sign Up</Link></li>
-                        <li><Link to="/signin">Sign In</Link></li>
-                    </ul>
-                    <ul className="header--signedin">
-                        <li>Welcome, Joe Smith!</li>
-                        <li><Link to="sign-out.html">Sign Out</Link></li> {/* TODO: make live link */}
-                    </ul>
+                <AuthConsumer>
+                    { context => {
+                        if(context.id && context.id !==0) {
+                            return (
+                                <ul className="header--signedin">
+                                    <li>Welcome, {context.firstName} {context.lastName}!</li>
+                                    <li><Link to="sign-out.html">Sign Out</Link></li> {/* TODO: make live link */}
+                                </ul>
+                            );
+                        } else {
+                            return (
+                                <ul className="header--signedout">
+                                    <li><Link to="/signup">Sign Up</Link></li>
+                                    <li><Link to="/signin">Sign In</Link></li>
+                                </ul>
+                            );
+                        }
+                    }}
+                </AuthConsumer>
                 </nav>
             </div>
         </header>
