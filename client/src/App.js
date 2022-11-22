b072e2a3ef0eca819e98d8a9af788fd361a67477
Will <wmchance@gmail.com>
2022-11-22 13:08:19 +0700

    make the create & update routes private

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ import UserSignUp from './components/UserSignUp';
 import CreateCourse from './components/CreateCourse';
 import UpdateCourse from './components/UpdateCourse';
 import Header from './components/Header';
-import Authenticated from "./components/Authenticated";
+// import Authenticated from "./components/Authenticated";
 
 function App() {
   const [userInfo, updateUserInfo] = useState({}); //Store user info from UserSignUp
@@ -57,24 +57,33 @@ function App() {
 
               <Route
                 path="/create-course"
-                element={<CreateCourse />}
+                element={
+                  <PrivateRoute>
+                    <CreateCourse />
+                  </PrivateRoute>
+                }
               />
 
               <Route
                 path="/:id/update"
-                element={<UpdateCourse />}
+                element={
+                  <PrivateRoute>
+                    <UpdateCourse />
+                  </PrivateRoute>
+                }
               />
               
-              {/* https://dev.to/iamandrewluca/private-route-in-react-router-v6-lg5
-              This link shows how to migrate from PrivateRoutes in React Router V.4 to PrivateRoutes in V.6 */}
-              <Route
+              {/* https://dev.to/iamandrewluca/private-route-in-react-router-v6-lg5 */}
+              {/* This link shows how to migrate from PrivateRoutes in React Router V.4 to PrivateRoutes in V.6 */}
+              {/* Below is an example to test that it's working */}
+              {/* <Route
                 path="/authenticated"
                 element={
                   <PrivateRoute>
                     <Authenticated />
                   </PrivateRoute>
                 }
-              />
+              /> */}
 
             </Routes>
           </main>
