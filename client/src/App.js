3c0ac7ba7aefce005573f93df2a770e5dff844c4
Will <wmchance@gmail.com>
2022-11-23 13:44:12 +0700

    change path: create-course -> create

--- client/src/App.js
+++ client/src/App.js
@@ -64,7 +64,7 @@ function App() {
               />
 
               <Route
-                path="/create-course"
+                path="/create"
                 element={
                   <PrivateRoute>
                     <CreateCourse />
